57db894be9fa9e61a948e34844d9928e59b7c1ca
jonas <jonasd1305@gmail.com>
2021-12-04 13:56:06 +0100

    day3 part 2 major optimization

--- day3.cpp
+++ day3.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+
 #include "AOC_Solver.h"
+#include <numeric>
 
 int64_t aoc::day3::part_1(std::vector<std::string>&input)
 {
@@ -30,53 +33,36 @@ int64_t aoc::day3::part_2(std::vector<std::string>&input)
 
 	char common_bit;
 	std::vector<std::string>oxygen{ input };
+	auto end_it = oxygen.end();
 	for (unsigned int i = 0; i < digits && oxygen.size()>1; ++i)
 	{
-		size_t oxygen_size = oxygen.size();
+		size_t oxygen_size = end_it-oxygen.begin();
 		if (oxygen_size == 1)continue;
-		std::vector<std::string>oxygen_tmp{};
 
-		int ones{ 0 };
-		for (auto& j : oxygen)
-		{
-			ones += (j[i] == '1' ? 1 : 0);
-			if(ones > (oxygen_size / 2))break;
-		}
+		int ones = std::accumulate(oxygen.begin(), end_it, 0,
+			[&](int& a, std::string& b) {return a + (b[i] == '1' ? 1 : 0); });
+
 		common_bit = (ones > (oxygen_size / 2) ? '1' : '0');
 		if (oxygen_size % 2 == 0 && oxygen_size / 2 == ones)common_bit = '1';
 
-		for (auto& j : oxygen)
-		{
-			const char digit = j[i];
-			if (digit == common_bit)
-				oxygen_tmp.push_back(j);
-		}
-
-		oxygen = oxygen_tmp;
+		end_it=std::remove_if(oxygen.begin(), end_it,
+			[&](std::string& a) {return a[i] != common_bit; });
 	}
-
-	std::vector<std::string>co2{ input };
-	for (unsigned int i = 0; i < digits && co2.size()>1; ++i)
+	
+	end_it = input.end();
+	for (unsigned int i = 0; i < digits && input.size()>1; ++i)
 	{
-		size_t co2_size = co2.size();
+		size_t co2_size = end_it - input.begin();
 		if (co2_size == 1)continue;
-		std::vector<std::string>co2_tmp{};
 
-		int ones{ 0 };
-		for (auto& j : co2)
-		{
-			ones += (j[i] == '1' ? 1 : 0);
-			if (ones > (co2_size / 2))break;
-		}
+		int ones = std::accumulate(input.begin(), end_it, 0, 
+			[&](int& a, std::string& b) {return a + (b[i] == '1' ? 1 : 0); });
+
 		common_bit = (ones > (co2_size / 2) ? '1' : '0');
 		if (co2_size % 2 == 0 && co2_size / 2 == ones)common_bit = '1';
 
-		for (auto& j : co2)
-		{
-			if (j[i] != common_bit)co2_tmp.push_back(j);
-		}
-
-		co2 = co2_tmp;
+		end_it = std::remove_if(input.begin(), end_it,
+			[&](std::string& a) {return a[i] == common_bit; });
 	}
-	return std::stoll(oxygen.front(), nullptr, 2) * std::stoll(co2.front(), nullptr, 2);
+	return std::stoll(oxygen[0], nullptr, 2) * std::stoll(input[0], nullptr, 2);
 }
