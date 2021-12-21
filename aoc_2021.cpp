33b547b7426d0ae82e0228b66b1e398737af8fee
Jonas <jonasd1305@gmail.com>
2021-12-21 07:50:07 +0100

    day 21 solved

--- aoc_2021.cpp
+++ aoc_2021.cpp
@@ -90,7 +90,7 @@ double benchmark_day(std::vector<INPUT> data, F1 part1, F2 part2, int day) {
 }
 
 #if NDEBUG
-constexpr int N = 25;
+constexpr int N = 1;//25;
 #endif
 #if !NDEBUG
 constexpr int N = 1;
@@ -103,6 +103,7 @@ int main()
 	double total_time{};
 #if NDEBUG
 	double day12{};
+	/*
 	total_time += benchmark_day<int, N>(input::data_as_int("Input/day1.txt"), aoc::day1::part_1, aoc::day1::part_2, 1);
 	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day2.txt"), aoc::day2::part_1, aoc::day2::part_2, 2);
 	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day3.txt"), aoc::day3::part_1, aoc::day3::part_2, 3);
@@ -123,9 +124,9 @@ int main()
 	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day18.txt"), aoc::day18::part_1, aoc::day18::part_2, 18);
 	total_time += benchmark_day<std::string, 1>(input::data_as_string("Input/day19.txt"), aoc::day19::part_1, aoc::day19::part_2, 19);
 	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day20.txt"), aoc::day20::part_1, aoc::day20::part_2, 20);
-
+	*/
 #endif
-	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day20.txt"), aoc::day21::part_1, aoc::day21::part_2, 21);
+	total_time += benchmark_day<std::string, N>(input::data_as_string("Input/day21.txt"), aoc::day21::part_1, aoc::day21::part_2, 21);
 
 	SetConsoleTextAttribute(hConsole, 2);
 	std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~~ Total ~~~~~~~~~~~~~~~~~~~~~~~\n";
--- day21.cpp
+++ day21.cpp
@@ -1,9 +1,59 @@
+#include <array>
+#include <iostream>
+
 #include "AOC_Solver.h"
 
 uint64_t aoc::day21::part_1(std::vector<std::string>& input) {
-	return 0;
+	int p1 = input[0][28] - 48;
+	int p2 = input[1][28] - 48;
+	int s1 = 0, s2 = 0;
+	int die = 1, rolls = 0;
+	bool turn_p1 = true;
+	while (s1 < 1000 && s2 < 1000) {
+
+		int die_value;
+		if (die <= 98)
+			die_value = 3 * die + 3;
+		else
+			die_value = die + ((die + 1) % 100) + ((die + 2) % 100);
+		die += 3;
+		rolls += 3;
+		if (die > 100)die -= 100;
+		if (turn_p1) {
+			p1 = p1 + die_value;
+			while (p1 > 10)p1 -= 10;
+			s1 += p1;
+		}
+		else {
+			p2 = p2 + die_value;
+			while (p2 > 10)p2 -= 10;
+			s2 += p2;
+		}
+		turn_p1 = !turn_p1;
+	}
+	int loosing = (s1 < 1000 ? s1 : s2);
+	return loosing * rolls;
+}
+
+std::pair<uint64_t, uint64_t> play(int p1,int p2,int s1,int s2) {
+	if(s2>=21) return std::make_pair(0,1);
+	uint64_t w1 = 0, w2 = 0;
+	int rolls[7][2] = { {3,1},{4,3},{5,6},{6,7},{7,6},{8,3},{9,1} };
+	for(auto comb:rolls)
+	{
+		int p = (p1 + comb[0]) % 10 ? (p1 + comb[0]) % 10 : 10;
+		auto [v2, v1] = play(p2, p, s2, s1 + p);
+		w1 = w1 + v1 * comb[1];
+		w2 = w2 + v2 * comb[1];
+
+	}
+	return std::make_pair(w1, w2);
 }
 
 uint64_t aoc::day21::part_2(std::vector<std::string>& input) {
-	return 0;
+	int p1 = input[0][28] - 48;
+	int p2 = input[1][28] - 48;
+	auto[wins_p1,wins_p2] = play(p1, p2, 0, 0);
+
+	return wins_p1>wins_p2?wins_p1:wins_p2;
 }
